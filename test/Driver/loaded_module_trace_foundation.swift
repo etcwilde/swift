e88b4aca6bb06dd634e0f88398a53316db670547
Evan Wilde <ewilde@apple.com>
2021-09-20 15:51:25 -0700

    XFail failing driver test for apple silicon
    https://ci.swift.org/view/Swift%20rebranch/job/oss-swift-rebranch-incremental-RA-macos-apple-silicon/23/
    
    Changes between the failing build and the last good build:
    
    Git (git git@github.com:apple/swift-continuous-integration.git)
    - Update the version — Commit 4797cab7965b2c2935bec207dbd043a04440b49d
    - Revert "Fix Swift Driver testing to swift-DEVELOPMENT-SNAPSHOT-2021-09-11-a tag" — Commit 3d234d98ff59b523775ffe5acd6b6c6e19267b88
    - Revert "Fix Swift Driver testing to swift-DEVELOPMENT-SNAPSHOT-2021-09-11-a tag" — Commit f4342bcc624524253e74d4aa1682900ecd843291
    
    Git (git git@github.com:apple/llvm-project.git)
    - Turn on swiftasync for i386. — ed1a8e179a33b57ffdac5b0f7b96949679f1a00e
    - [MachCore] Report arm64 thread exception state — 7b1f0cb5f36680c1e0f3f56c38cedf1a3162a21a
    - [lldb][crashlog] Avoid specifying arch for image when a UUID is present — 5966f3eb5785f9f1deb26f33c41e248646793281
    
    Git (git git@github.com:apple/swift-syntax.git)
    - Add convenience initializers to `VariableDecl` — 150dbfac525cdb2bd5ea5eb743fe4f32912846db
    
    Git (git git@github.com:apple/swift-driver.git)
    - [Dependency Scanning] Do not include PCH-handling command-line flags on the scanning jobs — 20cbf5ea95d4c65f35ef02fe60efaf21a377d2d9
    
    Git (swift)
    - [CodeCompletion] Fix a crash in solver-based keypath completion — 09a073c750dae43228853aaf7dae543313ac9a10
    - Update WindowsBuild.md — e0959541fdcb76cb0b740c49b8bbfaf4d3852af0
    - Use vsnprintf instead of vsprintf in Portability.h (#39366) — bc1647a6dcd4d07f316adbbbcac5f59f72809a1c
    - Fix comment about LTO and autolinking in lit.cfg (#39362) — 17d33353059f680fb1c0713194b0aa622e3dd67a
    - libswift: fix a few bugs in StackList — 50195e7e7d165b4993101a01e4fba739ec6d33ce
    - Enable late OME via an experimental flag (#39363) — d7281a1001950e62f592599519a6ab06bad4262b
    
    rdar://83328085

--- test/Driver/loaded_module_trace_foundation.swift
+++ test/Driver/loaded_module_trace_foundation.swift
@@ -1,6 +1,7 @@
 // RUN: %target-build-swift -o %t -module-name loaded_module_trace_foundation %s -emit-loaded-module-trace -emit-loaded-module-trace-path - 2>&1 | %FileCheck %s
 
 // REQUIRES: objc_interop
+// REQUIRES: rdar://83328085
 
 // CHECK: {
 // CHECK: "name":"loaded_module_trace_foundation"
