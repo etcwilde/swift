6b1b17f4894ddf0e931bb2ce0d416ecfd8062ea7
Evan Wilde <etceterawilde@gmail.com>
2022-03-03 22:19:59 -0800

    Names, uniqueness, and file types
    Fixing the names of the tables to consistently be singular.
    Adding some uniqueness requirements to the table.
    Adding file types table to map the type of file to a string describing
    the type of the file.

--- utils/toolchain-size/schema.sql
+++ utils/toolchain-size/schema.sql
@@ -2,23 +2,31 @@ PRAGMA foreign_keys = ON;
 
 CREATE TABLE IF NOT EXISTS platform (
   pid  INTEGER PRIMARY KEY,   -- platform id
-  name TEXT                   -- platform name (windows, linux, macos)
+  name TEXT UNIQUE            -- platform name (windows, linux, macos)
 );
 
 CREATE TABLE IF NOT EXISTS toolchain (
   tid  INTEGER PRIMARY KEY,   -- toolchain id
-  name TEXT                   -- toolchain name
+  name TEXT UNIQUE            -- toolchain name
 );
 
 CREATE TABLE IF NOT EXISTS sdk (
   sid  INTEGER PRIMARY KEY,   -- sdk id
-  name TEXT                   -- sdk name
+  name TEXT UNIQUE            -- sdk name
 );
 
-CREATE TABLE IF NOT EXISTS files (
-  fid  INTEGER PRIMARY KEY,   -- file id
-  path text                   -- file path
+CREATE TABLE IF NOT EXISTS file_type (
+  tid  INTEGER PRIMARY KEY,   -- file type ID
+  name TEXT UNIQUE            -- name of the filetype (file, directory, symlink)
 );
+
+CREATE TABLE IF NOT EXISTS file (
+  fid  INTEGER PRIMARY KEY,   -- file id
+  filetype INTEGER,           -- type of the file
+  path text UNIQUE,           -- file path
+  FOREIGN KEY(filetype) REFERENCES file_type(tid)
+);
+
 
 CREATE TABLE IF NOT EXISTS build_info (
   bid  INTEGER PRIMARY KEY,   -- build id
@@ -34,25 +42,25 @@ CREATE TABLE IF NOT EXISTS size (
   bytes INTEGER,              -- number of bytes in the file for this build
   PRIMARY KEY(bid, fid),
   FOREIGN KEY(bid) REFERENCES build_info(bid),
-  FOREIGN KEY(fid) REFERENCES files(fid)
+  FOREIGN KEY(fid) REFERENCES file(fid)
 );
 
-CREATE TABLE IF NOT EXISTS toolchain_files (
+CREATE TABLE IF NOT EXISTS toolchain_file (
   tid  INTEGER,               -- toolchain id
   fid  INTEGER,               -- file id
   bid  INTEGER,               -- build id
   PRIMARY KEY(tid, fid, bid),
   FOREIGN KEY(tid) REFERENCES toolchain(tid),
-  FOREIGN KEY(fid) REFERENCES files(fid),
+  FOREIGN KEY(fid) REFERENCES file(fid),
   FOREIGN KEY(bid) REFERENCES build_info(bid)
 );
 
-CREATE TABLE IF NOT EXISTS sdk_files (
+CREATE TABLE IF NOT EXISTS sdk_file (
   sid  INTEGER,               -- SDK id
   fid  INTEGER,               -- file id
   bid  INTEGER,               -- build id
   PRIMARY KEY(sid, fid, bid),
   FOREIGN KEY(sid) REFERENCES sdk(sid),
-  FOREIGN KEY(fid) REFERENCES files(fid),
+  FOREIGN KEY(fid) REFERENCES file(fid),
   FOREIGN KEY(bid) REFERENCES build_info(bid)
 );
